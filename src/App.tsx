3537fd9df51c90e6b6a32a12915f10b1d342341f
brianirons <brian.irons@newsclip.co.za>
2021-09-23 08:00:48 +0200

    added drawing canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DrawAnnotations from './comconents/drawing-canvas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is a persistent drawing area example</h1>
+      <DrawAnnotations></DrawAnnotations>
     </div>
   );
 }
