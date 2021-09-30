dc8cb0162b383ff1722dc6c36d5b621e5d00d286
Brian Irons <the.brian.irons@gmail.com>
2021-09-30 14:20:25 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,27 @@ import { Switch, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import React from 'react';
 import './App.css';
 import DrawAnnotations from './comconents/drawing-canvas';
-import { ChangeState } from './drawState';
 
 function App() {
-<<<<<<< HEAD
-  const [alignment, setAlignment] = React.useState('web');
-  const handleChange = (event: any, newAlignment: any) => {
-    setAlignment(newAlignment);
-    return (
-      <div>
-        <h1>This is a persistent drawing area example</h1>
-        <ToggleButtonGroup
-          color="primary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-        >
-          <ToggleButton value="web">Create</ToggleButton>
-          <ToggleButton value="android">Edit</ToggleButton>
-        </ToggleButtonGroup>
-        <DrawAnnotations></DrawAnnotations>
-      </div>
-    );
-  }
-=======
-  return (
-    <div>
-      <h1>This is a persistent drawing area example</h1>
-      <ChangeState></ChangeState>
-      <DrawAnnotations></DrawAnnotations>
-    </div>
-  );
+	const [alignment, setAlignment] = React.useState('web');
+	const handleChange = (event: any, newAlignment: any) => {
+		setAlignment(newAlignment);
+		return (
+			<div>
+				<h1>This is a persistent drawing area example</h1>
+				<ToggleButtonGroup
+					color="primary"
+					value={alignment}
+					exclusive
+					onChange={handleChange}
+				>
+					<ToggleButton value="web">Create</ToggleButton>
+					<ToggleButton value="android">Edit</ToggleButton>
+				</ToggleButtonGroup>
+				<DrawAnnotations></DrawAnnotations>
+			</div>
+		);
+	}
 }
->>>>>>> 1de9528677d929e58431dc9baf067cd99ab2aaf2
 
-  export default App;
+export default App;
